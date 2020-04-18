b63e4a861dbaa150b9942e387ba6cfd4854ee05a
keith <kisham8899@gmail.com>
2020-04-18 14:33:56 -0400

    AI
    Basic map.
    EnemyController --- AI finds player OR goes to endpoint. More to come.

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -1,26 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
 
     public float lookRadius = 10f;
+    public GameObject endPoint;
+    public Transform target;
+   
+    NavMeshAgent agent;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        //target = PlayerManager1.instance.playerObj.transform;
         
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+        }
+        else 
+        { 
+            agent.SetDestination(endPoint.transform.position);
+        }
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        //Gizmos.DrawWireSphere
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
     }
 }
